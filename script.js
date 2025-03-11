b977cac712b8ea44754786091bb6142b5b428fc1
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-03-11 09:22:39 +0100

    Capitalise first letter of location name and convert temperatures to celsius

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function capitaliseFirstLetter(val) {
+  return String(val).charAt(0).toUpperCase() + String(val).slice(1)
+}
+
 async function getWeather() {
   const url =
     'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/paris?key=DRYKFVEYKQSZTNND2L3L2TKAF'
@@ -10,12 +14,14 @@ async function getWeather() {
 
     const data = await response.json()
 
-    const location = data.address
+    const location = capitaliseFirstLetter(data.address)
     const today = data.days[0]
+    const temperature = ((today.temp - 32) * 5) / 9
+    const feelsLike = ((today.feelslike - 32) * 5) / 9
 
     console.log(`📍 Location: ${location}`)
-    console.log(`🌡️ Temperature: ${today.temp}°C`)
-    console.log(`🤒 Feels Like: ${today.feelslike}°C`)
+    console.log(`🌡️ Temperature: ${temperature}°C`)
+    console.log(`🤒 Feels Like: ${feelsLike}°C`)
     console.log(`🌧️ Precipitation Probability: ${today.precipprob}%`)
     console.log(`⛅ Conditions: ${today.conditions}`)
     console.log(`🖼️ Icon: ${today.icon}`)
